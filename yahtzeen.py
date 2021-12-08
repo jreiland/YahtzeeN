3b794a81eee00b4dba696ffca3aea447840d729d
John Reiland <jreiland1@pride.hofstra.edu>
2021-12-08 08:25:55 -0500

    Updated UI

--- yahtzeen.py
+++ yahtzeen.py
@@ -23,12 +23,18 @@ PURPLE = (96, 20,226)
 YELLOW = (234, 216, 65)
 BROWN = (119, 77, 43)
 PINK = (248, 179, 197)
-#temp to test image
-LEFT= pygame.Rect(width/10, height/4, 120, 120)
+
+#temp positions of six dice (to be used for upper section)
+LEFT= pygame.Rect((width/10), height/2, 120, 120)
+LESS_LEFT = pygame.Rect((width/10)+75, height/2, 120, 120)
+LESSER_LEFT= pygame.Rect((width/10)+150, height/2, 120, 120)
+EVEN_LESS_LEFT = pygame.Rect((width/10)+225, height/2, 120, 120)
+LEFFFFFFFT= pygame.Rect((width/10)+300, height/2, 120, 120)
+MOST_LEFT = pygame.Rect((width/10)+375, height/2, 120, 120)
 
 def displayPicture(picture,location):
     image = pygame.image.load(picture).convert_alpha()
-    image = pygame.transform.rotozoom(image, 0, 0.2)
+    image = pygame.transform.rotozoom(image, 0, 0.087081)
     surface.blit(image, location)
 
 def main():
@@ -42,10 +48,18 @@ def main():
 
 
 
-        #set background color
+        #draw UI!
         surface.fill(BKGR_RED)
 
-        displayPicture("ace.png", LEFT)
+        pygame.draw.line(surface, BLACK, (0, 200),(width, 200),2) #top third
+        pygame.draw.line(surface, BLACK, (0, 550),(width, 550),2) #bottom third
+        pygame.draw.line(surface, BLACK, (500,0), (500, 200),  2) #top third vertical line
+        displayPicture("six.png", LEFT)
+        displayPicture("ace.png", LESS_LEFT)
+        displayPicture("two.png", LESSER_LEFT)
+        displayPicture("three.png", EVEN_LESS_LEFT)
+        displayPicture("four.png", LEFFFFFFFT)
+        displayPicture("five.png", MOST_LEFT)
 
         pygame.display.update()
 main()
