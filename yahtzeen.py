596abc83415a201a1517dee138290d149f1bd548
John Reiland <jreiland1@pride.hofstra.edu>
2021-12-08 15:59:27 -0500

    UI without text and upper dice

--- yahtzeen.py
+++ yahtzeen.py
@@ -13,7 +13,7 @@ surface = pygame.display.set_mode(size)
 pygame.display.set_caption("YahtzeeN")
 
 #color constants
-GREEN = (0, 255, 0)
+GREEN = (27, 105, 3)
 BLUE = (0, 0, 255)
 BKGR_RED = (148, 10, 10)
 WHITE = (255, 255, 255)
@@ -54,9 +54,24 @@ def main():
         pygame.draw.line(surface, BLACK, (0, 700/3),(width, 700/3),2) #top third
         pygame.draw.line(surface, BLACK, (0, 2*(700/3)),(width, 2*(700/3)),2) #bottom third
         pygame.draw.line(surface, BLACK, (500,0), (500, 700/3),  2) #top third vertical line
-        pygame.draw.line(surface, BLACK, (500, 58), (width, 58), 2)
-        pygame.draw.line(surface, BLACK, (500, 116), (width, 116), 2)
-        pygame.draw.line(surface, BLACK, (500, 174), (width, 174), 2)
+        pygame.draw.line(surface, BLACK, (500, 58), (width, 58), 2) #top logo/scoring line
+        pygame.draw.line(surface, BLACK, (500, 116), (width, 116), 2) #second scoring line
+        pygame.draw.line(surface, BLACK, (500, 174), (width, 174), 2) #third scoring line
+
+        #draw player indicator box
+        pygame.draw.line(surface, BLACK, (300,0), (300, 75), 2)
+        pygame.draw.line(surface, BLACK, (0, 75), (300, 75), 2)
+
+        #miscellaneous UI elements
+        pygame.draw.line(surface, BLACK, (820, 0), (820, 58), 2)
+        pygame.draw.line(surface, BLACK, (840, 20), (880, 20), 4) #menu line one
+        pygame.draw.line(surface, BLACK, (840, 30), (880, 30), 4) #menu line two
+        pygame.draw.line(surface, BLACK, (840, 40), (880, 40), 4) #menu line three
+        pygame.draw.rect(surface, GREEN, pygame.Rect(310, 10, 182, 60), 0, 3) #rect is at (310, 10), width 182, height 60px
+
+        #display dice for current roll, need to be 28.88 pixels each with 10 px between each
+
+
         displayPicture("ace.png", LEFT)
         displayPicture("two.png", LESS_LEFT)
         displayPicture("three.png", LESSER_LEFT)
