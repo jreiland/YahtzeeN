888ae8cfbda610f896513f2c9b4523095935931c
John Reiland <jreiland1@pride.hofstra.edu>
2021-12-08 20:16:57 -0500

    All UI done minus words

--- yahtzeen.py
+++ yahtzeen.py
@@ -13,28 +13,31 @@ surface = pygame.display.set_mode(size)
 pygame.display.set_caption("YahtzeeN")
 
 #color constants
-GREEN = (27, 105, 3)
-BLUE = (0, 0, 255)
+GREEN =    (27, 105, 3)
+BLUE =     (0, 0, 255)
 BKGR_RED = (148, 10, 10)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-FUN = (231, 78, 111)
-PURPLE = (96, 20,226)
-YELLOW = (234, 216, 65)
-BROWN = (119, 77, 43)
-PINK = (248, 179, 197)
+WHITE =    (255, 255, 255)
+BLACK =    (0, 0, 0)
+PURPLE =   (96, 20, 226)
+BROWN =    (119, 77, 43)
 
 #temp positions of six dice (to be used for upper section)
-LEFT= pygame.Rect((width/10)+50, height/2.25, 120, 120)
+LEFT = pygame.Rect((width/10)+50, height/2.25, 120, 120)
 LESS_LEFT = pygame.Rect((width/10)+150, height/2.25, 120, 120)
-LESSER_LEFT= pygame.Rect((width/10)+250, height/2.25, 120, 120)
+LESSER_LEFT = pygame.Rect((width/10)+250, height/2.25, 120, 120)
 EVEN_LESS_LEFT = pygame.Rect((width/10)+350, height/2.25, 120, 120)
-LEFFFFFFFT= pygame.Rect((width/10)+450, height/2.25, 120, 120)
+LEFFFFFFFT = pygame.Rect((width/10)+450, height/2.25, 120, 120)
 MOST_LEFT = pygame.Rect((width/10)+550, height/2.25, 120, 120)
 
-def displayPicture(picture,location):
+TOP_LEFT = pygame.Rect(30, 120, 120, 120)
+TOP_LESS_LEFT = pygame.Rect(120, 120, 120, 120)
+TOP_LESSER_LEFT = pygame.Rect(210, 120, 120, 120)
+TOP_EVEN_LESS_LEFT = pygame.Rect(300, 120, 120, 120)
+TOP_LEFFFFFFFT = pygame.Rect(390, 120, 120, 120)
+
+def displayPicture(picture,location,scale):
     image = pygame.image.load(picture).convert_alpha()
-    image = pygame.transform.rotozoom(image, 0, 0.087081)
+    image = pygame.transform.rotozoom(image, 0, scale)
     surface.blit(image, location)
 
 def main():
@@ -63,21 +66,26 @@ def main():
         pygame.draw.line(surface, BLACK, (0, 75), (300, 75), 2)
 
         #miscellaneous UI elements
-        pygame.draw.line(surface, BLACK, (820, 0), (820, 58), 2)
+        pygame.draw.line(surface, BLACK, (820, 0), (820, 58), 2) #vertical menu line
         pygame.draw.line(surface, BLACK, (840, 20), (880, 20), 4) #menu line one
         pygame.draw.line(surface, BLACK, (840, 30), (880, 30), 4) #menu line two
         pygame.draw.line(surface, BLACK, (840, 40), (880, 40), 4) #menu line three
         pygame.draw.rect(surface, GREEN, pygame.Rect(310, 10, 182, 60), 0, 3) #rect is at (310, 10), width 182, height 60px
 
-        #display dice for current roll, need to be 28.88 pixels each with 10 px between each
+        #display dice for current roll, need to be 80 pixels each with 10 px between each
+        displayPicture("ace.png", TOP_LEFT, 0.076555)
+        displayPicture("two.png", TOP_LESS_LEFT, 0.076555)
+        displayPicture("three.png", TOP_LESSER_LEFT, 0.076555)
+        displayPicture("four.png", TOP_EVEN_LESS_LEFT, 0.076555)
+        displayPicture("five.png", TOP_LEFFFFFFFT, 0.076555)
 
-
-        displayPicture("ace.png", LEFT)
-        displayPicture("two.png", LESS_LEFT)
-        displayPicture("three.png", LESSER_LEFT)
-        displayPicture("four.png", EVEN_LESS_LEFT)
-        displayPicture("five.png", LEFFFFFFFT)
-        displayPicture("six.png", MOST_LEFT)
+        #display middle dice
+        displayPicture("ace.png", LEFT, 0.087081)
+        displayPicture("two.png", LESS_LEFT, 0.087081)
+        displayPicture("three.png", LESSER_LEFT, 0.087081)
+        displayPicture("four.png", EVEN_LESS_LEFT, 0.087081)
+        displayPicture("five.png", LEFFFFFFFT, 0.087081)
+        displayPicture("six.png", MOST_LEFT, 0.087081)
 
         pygame.display.update()
 main()
